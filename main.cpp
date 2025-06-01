0678c0c161072567d0ee412184dfd6a1db2a70f4
KayKayDoubleu <lotheruyt@gmail.com>
2025-06-01 13:11:35 +0200

    klasa i funkcje

--- main.cpp
+++ main.cpp
@@ -3,10 +3,44 @@
 
 using namespace std;
 
+class Konto{
+    private:
+             string typek;
+             double saldo;
+    public:
+            Konto(string imie, double saldoStartowe){
+                typek = imie;
+                saldo = saldoStartowe;
+            }
+        
+    void wplata(double kwota){
+        if(kwota>0)
+        {
+            saldo += kwota;
+            cout<<"Wpłacono: "<<kwota<<" na konto "<<typek<<"."<<endl;
+        }
+    }
+    
+    void wyplata(double kwota){
+        if(kwota>0 && saldo>=kwota)
+        {
+            saldo -= kwota;
+            cout<<"Wypłacono: "<<kwota<< " z konta "<< typek<<"."<<endl;
+        }
+    }
+    void pokazSaldo() {
+        cout<<"Saldo konta "<<typek<< "jest równe: "<<saldo<<"."<<endl;
+    }
+    };
 
 int main(){
 
-    cout<<"skibidi";
+    Konto konto1("Konrad", 1000);
+    konto1.wplata(100);
+    konto1.wplata(10000);
+    konto1.wyplata(100);
+    konto1.pokazSaldo();
+    konto1.wyplata(100000);
 
 
 
